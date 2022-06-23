2ea8cbbb27d7756946dbc18d23c174d005290ac4
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-23 11:18:28 +0000

    updated

--- map.js
+++ map.js
@@ -1,3 +1,16 @@
+/*Create a function subtractTwo that accepts a number and returns that number minus 2.
+
+Then create a function map that takes two inputs -
+
+an array of numbers (a list of numbers)
+a 'callback' function - this function is applied to each element of the array (inside of the function 'map')
+Have your map function return a new array filled with numbers that are the result of using the 'callback' function on each element of the input array. Please do not use the native map or forEach method.
+
+map([3,4,5], subtractTwo); //-> [1,2,3]
+subtractTwo(10); //-> 8
+subtractTwo(12); //-> 10
+*/
+
 function subtractTwo(num) {
   return num - 2;
 }
